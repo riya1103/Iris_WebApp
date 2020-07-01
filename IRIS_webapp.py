aa8862870401a2b7dce4827be1e62045426be400
Riya Jain <46837551+riya1103@users.noreply.github.com>
2020-07-01 16:05:55 +0530

    Add files via upload

---
+++ IRIS_webapp.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import streamlit as st
+from sklearn import datasets
+
+iris = datasets.load_iris()
+X = iris.data
+Y = iris.target
+
+from sklearn.neighbors import KNeighborsClassifier
+clf = KNeighborsClassifier(n_neighbors = 20, metric = 'minkowski', p = 2)
+clf.fit(X, Y)
+
+
+st.write("""
+# Simple Iris Flower Prediction App
+This app predicts the **Iris flower** type!
+""")
+
+st.sidebar.header("User Imput Parameters")
+
+def user_input_features():
+    sepal_length = st.sidebar.slider("Sepal lenght",4.3,7.9,5.4)
+    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
+    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
+    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
+    data = {'sepal_length': sepal_length,
+            'sepal_width': sepal_width,
+            'petal_length': petal_length,
+            'petal_width': petal_width}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
+
+
+st.subheader('Class labels and their corresponding index number')
+st.write(iris.target_names)
+
+st.subheader('Prediction')
+st.write(iris.target_names[prediction])
+#st.write(prediction)
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
